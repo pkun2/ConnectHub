35670764191ea8e2b6a55e97b06d8d331b224b53
pkun2 <pkun2@naver.com>
2024-05-12 15:42:59 +0900

    조회 틀 생성 및 모델 promise 문법으로 변경

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -6,11 +6,35 @@ export const getWriteController = (req, res) => {
 
 export const postWriteController = (req, res) => {
     const { userId, categoryId, title, content } = req.body; // Request Body에서 userId, categoryId, title, content 추출
-    Post.insertPost(userId, categoryId, title, content, (err, result) => {
-        if (err) {// 삽입중 오류 발생시
-            res.status(500).send('Error creating post'); // 500: Internal Server Error
-            return;
-        }
-        res.status(200).send('Post created successfully'); // 200: OK
+    Post.insertPost(userId, categoryId, title, content)
+        .then(result => {
+            // 삽입 성공시
+            res.status(200).send('게시글이 성공적으로 생성되었습니다.', result); // 200: OK
+            
+        })
+        .catch(err => {
+            // 삽입중 오류 발생시
+            res.status(500).send('게시글 생성중 오류발생', err); // 500: Internal Server Error
+        });
+}
+
+export const postViewController = (req, res) => {
+    //mysql에서 post된 글들 가져와야 함. 카테고리별 분리할 부분도 필요하고,
+    //페이지네이션도 필요함.
+    // postId
+    // userId
+    // categoryId
+    // title
+    // content
+    // createdAt
+    Post.getAllPosts()
+    .then(posts => {
+        // Handle successful retrieval
+        res.status(200).send('게시글 조회 성공', posts);
+    })
+    .catch(err => {
+        // Handle error
+        res.status(500).send('게시글 조회중 오류 발생', err);
     });
+
 }
--- backend/model/Post.js
+++ backend/model/Post.js
@@ -1,18 +1,34 @@
 import db from '../config/db.js';
 
 class Post {
-    static insertPost(userId, categoryId, title, content, callback) {
-        const sql = 'INSERT INTO posts (userId, categoryId, title, content) VALUES (?, ?, ?, ?)';
-        const values = [userId, categoryId, title, content];
+    static insertPost(userId, categoryId, title, content) {
+        return new Promise((resolve, reject) => {
+            const sql = 'INSERT INTO posts (userId, categoryId, title, content) VALUES (?, ?, ?, ?)';
+            const values = [userId, categoryId, title, content];
         
-        db.query(sql, values, (err, result) => {
-            if (err) {
-                console.error('게시글 삽입중 오류 발생:', err);
-                callback(err, null);
-                return;
-            }
-            callback(null, result);
-            console.log('게시글이 생성되었습니다');
+            db.query(sql, values, (err, result) => {
+                if (err) {
+                    console.error('게시글 삽입중 오류 발생:', err);
+                    reject(err);
+                    return;
+                }
+                resolve(result);
+                console.log('게시글이 생성되었습니다');
+            });
+        });
+    }
+
+    static getAllPosts() {
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM posts';
+            db.query(sql, (err, result) => {
+                if (err) {
+                    console.error('게시글 조회중 오류 발생:', err);
+                    reject(err);
+                    return;
+                }
+                resolve(result);
+            });
         });
     }
 }
