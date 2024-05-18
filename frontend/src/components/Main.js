0d14fc30223bb9a77eb7ec10fc7a5d1c91478da8
YongJun Yun <a86622014@gmail.com>
2024-05-18 18:02:41 +0900

    메인 홈페이지 5차 수정

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Navigation from './Navigation';
 import Option from './Option';
@@ -7,19 +7,17 @@ import BoardSection from './BoardSection';
 import ProfileSection from './ProfileSection';
 import MenuSection from './MenuSection';
 
-// 메인 컨테이너
 const MainContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
-  height: 1503px;
+  height: 1550px;
   padding-top: 0px;
   margin: 0 200px 100px 200px;
   overflow-x: auto;
   overflow-y: hidden;
 `;
 
-// 좌측 컨테이너
 const LeftSubContainer = styled.div`
   flex: 1;
   display: flex;
@@ -29,7 +27,6 @@ const LeftSubContainer = styled.div`
   margin: 20px 20px;
 `;
 
-// 우측 컨테이너
 const RightSubContainer = styled.div`
   flex: 0.3;
   display: flex;
@@ -40,22 +37,30 @@ const RightSubContainer = styled.div`
 `;
 
 const Main = () => {
-  // 비어있는 컨텐츠를 만듭니다.
-  const numberOfEmptyContents = 18;
+  const numberOfEmptyContents = 55;
   const emptyContents = Array.from({ length: numberOfEmptyContents }, (_, index) => ({ id: index }));
+
+  const [selectedCategory, setSelectedCategory] = useState('전체게시판');
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
 
   return (
     <>
-      <Navigation/>
-      <Option/>
+      <Navigation />
+      <Option />
       <MainContainer>
         <LeftSubContainer>
           <ImageSection imageUrl="https://img.freepik.com/free-vector/men-women-welcoming-people-with-disabilities-group-people-meeting-blind-female-character-male-wheelchair_74855-18436.jpg?t=st=1715345864~exp=1715349464~hmac=174d5e762b369d4beba592670b688d3510807248c829290eee0a091388aae385&w=826" />
-          <BoardSection title="전체 게시판" contents={emptyContents} />
+          <BoardSection title={selectedCategory} contents={emptyContents} onCategoryChange={handleCategoryChange} />
         </LeftSubContainer>
         <RightSubContainer>
           <ProfileSection />
-          <MenuSection />
+          <MenuSection
+            onCategoryChange={handleCategoryChange}
+            selectedCategory={selectedCategory}
+          />
         </RightSubContainer>
       </MainContainer>
     </>
