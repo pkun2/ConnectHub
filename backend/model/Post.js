79484d4fa6641b078c509d632a843cfc64c20299
pkun <pkun2018@gmail.com>
2024-05-21 22:27:35 +0900

    계속 connection이 이뤄져야하는 db 부분, Postmode 리팩토링 진행

--- backend/model/Post.js
+++ backend/model/Post.js
@@ -1,61 +1,41 @@
-import connectToDatabase from '../config/db.js';
+import pool from "../config/db.js"
 
 class Post {
+    // 게시글 삽입
     static async insertPost(userId, categoryId, title, content) {
-        const connection = await connectToDatabase();
+        const query = 'INSERT INTO posts (userId, categoryId, title, content) VALUES (?, ?, ?, ?)';
+        const values = [userId, categoryId, title, content];
         try {
-            const query = 'INSERT INTO posts (userId, categoryId, title, content) VALUES (?, ?, ?, ?)';
-            const [result] = await connection.query(query, [userId, categoryId, title, content]);
+            const [result] = await pool.query(query, values);
             return result;
         } catch (err) {
-            console.log("삽입중 에러발생:", err);
             throw err;
-        } finally {
-            connection.end();
         }
     }
 
+    // 모든 게시글 조회
     static async getAllPosts(limit) {
-        const connection = await connectToDatabase();
+        const query = 'SELECT * FROM posts LIMIT ?';
+        const params = [parseInt(limit, 10)];
         try {
-            const query = "SELECT * FROM posts LIMIT ?";
-            const [result] = await connection.query(query, [parseInt(limit)]);
+            const [result] = await pool.query(query, params);
             return result;
         } catch (err) {
-            console.log("조회중 에러발생:", err);
             throw err;
-        } finally {
-            connection.end();
         }
     }
 
+    // 카테고리별 게시글 조회
     static async getPostByCategoryId(categoryId, limit) {
-        const connection = await connectToDatabase();
+        const query = 'SELECT * FROM posts WHERE categoryId = ? LIMIT ?';
+        const params = [categoryId, parseInt(limit, 10)];
         try {
-            const query = "SELECT * FROM posts WHERE categoryId = ? LIMIT ?";
-            const [result] = await connection.query(query, [categoryId, parseInt(limit)]);
+            const [result] = await pool.query(query, params);
             return result;
         } catch (err) {
-            console.log("조회중 에러발생:", err);
             throw err;
-        } finally {
-            connection.end();
-        }
-    }
-
-    static async getPostById(postId) {
-        const connection = await connectToDatabase();
-        try {
-            const query = "SELECT * FROM posts WHERE postId = ?";
-            const [result] = await connection.query(query, [postId]);
-            return result;
-        } catch (err) {
-            console.log("조회중 에러발생:", err);
-            throw err;
-        } finally {
-            connection.end();
         }
     }
 }
 
-export default Post;
+module.exports = Post;
