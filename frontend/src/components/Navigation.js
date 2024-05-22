da2993c41557b353148c4e1778db6ae7069641f0
JY <duawodud12@naver.com>
2024-05-22 23:00:12 +0900

    메인홈페이지 6차 수정
    - 알림 수신(종 모양), 알림 설정(톱니바퀴) 아이콘 추가
    - 알림 수신 버튼 클릭 시 전체화면이 흐리게 보이고 알림창이 가운데 뜨도록 구현
    - 닫기 버튼 및 알림창이 아닌 흐린 화면 클릭 시 메인화면으로 되돌아옴.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 // 네비게이션 바
@@ -40,11 +40,81 @@ const NavTitle = styled(NavItem)`
   font-size: 30pt;
 `;
 
+// 알림아이콘
+const NotificationIcon = styled.img`
+  position: absolute;
+  width: 50px;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 140px;
+  margin: 0;
+  cursor: pointer;
+`;
+
+// 알림설정아이콘
+const NotificationOptionIcon = styled.img`
+  position: absolute;
+  width: 50px;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 60px;
+  margin: 0;
+  cursor: pointer;
+`;
+
+// 전체 화면을 흐리게 만드는 오버레이
+const Modal_Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 반투명 검정색 */
+  z-index: 999; /* 모달보다 낮은 레벨 */
+`;
+
+// 알림창
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  z-index: 1000; /* 가장 높은 레벨 */
+  width: 500px;
+  height: 800px;
+`;
+
 function Navigation() {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <NavContainer>
       <NavTitle onClick={() => window.location.href='http://localhost:3000/'}> ConnectHub </NavTitle>
-      <div/> <div/> <div/> <div/> <div/> <div/> <div/>
+      <div/> <div/> <div/> <div/>
+      <NotificationIcon onClick={handleOpenModal} src='https://cdn.icon-icons.com/icons2/1993/PNG/512/alarm_alert_attention_bell_clock_notification_ring_icon_123203.png'/>
+      {showModal && (
+        <>
+          <Modal_Overlay onClick={handleCloseModal} />
+          <Modal>
+            <button onClick={handleCloseModal}>닫기</button>
+          </Modal>
+        </>
+      )}
+      <NotificationOptionIcon src='https://cdn.icon-icons.com/icons2/3106/PNG/512/gear_settings_options_icon_191642.png' alt='옵션' />
+      <div/>
     </NavContainer>
   );
 }
