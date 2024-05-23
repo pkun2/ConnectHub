da1f3128c6dd16e220fa03a2fdab47798f8954a9
JY <duawodud12@naver.com>
2024-05-23 23:01:02 +0900

    1. 페이지 제목 및 아이콘 변경
    2. 메인홈페이지 7차 수정
    
    - 알림 창 내부 구현
    - 알림 설정 창 구현
    - 새 게시글, 댓글, 메시지 알림 설정을 할건지 말건지에 대한 토글 버튼 구현

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Notification from './Notification';
 
 // 네비게이션 바
 const NavContainer = styled.nav`
@@ -27,9 +28,6 @@ const NavItem = styled.div`
   padding : 20px 20px;
   text-align : center;
   cursor: pointer;
-  &:hover {
-    box-shadow: 0 0 10px #44C2FF;
-  }
 `;
 
 // 제목
@@ -40,80 +38,23 @@ const NavTitle = styled(NavItem)`
   font-size: 30pt;
 `;
 
-// 알림아이콘
-const NotificationIcon = styled.img`
-  position: absolute;
-  width: 50px;
-  top: 50%;
-  transform: translateY(-50%);
-  right: 140px;
-  margin: 0;
-  cursor: pointer;
-`;
-
-// 알림설정아이콘
-const NotificationOptionIcon = styled.img`
-  position: absolute;
-  width: 50px;
-  top: 50%;
-  transform: translateY(-50%);
-  right: 60px;
-  margin: 0;
-  cursor: pointer;
-`;
-
-// 전체 화면을 흐리게 만드는 오버레이
-const Modal_Overlay = styled.div`
-  position: fixed;
-  top: 0;
+// 제목 아이콘
+const NavTitleIcon = styled.img`
+  position: relative;
   left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* 반투명 검정색 */
-  z-index: 999; /* 모달보다 낮은 레벨 */
-`;
-
-// 알림창
-const Modal = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  z-index: 1000; /* 가장 높은 레벨 */
-  width: 500px;
-  height: 800px;
+  margin-right: 35px;
 `;
 
 function Navigation() {
 
-  const [showModal, setShowModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <NavContainer>
-      <NavTitle onClick={() => window.location.href='http://localhost:3000/'}> ConnectHub </NavTitle>
-      <div/> <div/> <div/> <div/>
-      <NotificationIcon onClick={handleOpenModal} src='https://cdn.icon-icons.com/icons2/1993/PNG/512/alarm_alert_attention_bell_clock_notification_ring_icon_123203.png'/>
-      {showModal && (
-        <>
-          <Modal_Overlay onClick={handleCloseModal} />
-          <Modal>
-            <button onClick={handleCloseModal}>닫기</button>
-          </Modal>
-        </>
-      )}
-      <NotificationOptionIcon src='https://cdn.icon-icons.com/icons2/3106/PNG/512/gear_settings_options_icon_191642.png' alt='옵션' />
+      <NavTitle onClick={() => window.location.href='http://localhost:3000/'}> 
+        <NavTitleIcon src="./mainlogo.png" />
+        ConnectHub 
+      </NavTitle>
+      <div/> <div/> <div/> <div/> <div/> 
+      <Notification/>
       <div/>
     </NavContainer>
   );
